3d7fc5379c569e72d501b70402002c4e1741ef1d
xseniia <khitroxs@gmail.com>
2019-03-27 22:29:21 +0300

    accordeon task done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
+    }
+  }
+
+  changeState() {
+    let temp = !this.state.isActive;
+    this.setState({isActive: temp});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(<div className='accordion'>
+      <div className='header'
+           onClick={this.changeState.bind(this)}>
+        <span className='header-title'>Accordion</span>
+        <span className='header-button'></span>
       </div>
-    );
+      {this.state.isActive && <div className='content'>
+        <span className='content-text'>Some text</span>
+      </div>}
+    </div>);
+    
   }
 }
 
