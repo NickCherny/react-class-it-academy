cbe19ed16040c03548ba83e35eaaa6e936ed5c4f
nick_cherny <cherny.web@gmail.com>
2019-03-27 23:29:51 +0300

    step-4

--- src/state-manager/index.js
+++ src/state-manager/index.js
@@ -21,4 +21,50 @@ function createStore(reducer, initialState = {}) {
   };
 }
 
-export default createStore;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'USER_REQUEST':
+      return {
+        ...state,
+        isLoading: true
+      };
+    case 'USER_ERROR':
+      return {
+        ...state,
+        data: null,
+        isLoading: false,
+        error: action.payload
+      };
+    case 'USER_RECIVE':
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload
+      }
+  }
+};
+
+const store = createStore(reducer, { isLoading: false });
+
+function log() {
+  console.log(store.getState());
+}
+
+console.log(store.getState());
+
+store.subscribe(log);
+
+
+store.dispatch({ type: 'USER_REQUEST' });
+
+setTimeout(() => {
+  store.dispatch({ type: 'USER_RECIVE', payload: { username: 'nick', role: 'admin', password: '123' } });
+}, 500);
+
+setTimeout(() => {
+  store.dispatch({ type: 'USER_REQUEST' });
+}, 1200);
+
+setTimeout(() => {
+  store.dispatch({ type: 'USER_ERROR', payload: { code: 404, message: 'Request failed' } });
+}, 2000);
