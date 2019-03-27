51a643bb4271f252e5f34e8f55d0d43256e50729
GrinningTroll <kostya_mackevich@mail.ru>
2019-03-27 21:53:21 +0300

    Dropdow done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
-import Tab from './components/Tab';
-
+import logo from './logo.svg';
+import './App.css';
+import Dropdown from "./Dropdown";
+const data = [
+  { key: 'A', text: 'Option a' },
+  { key: 'B', text: 'Option b' },
+  { key: 'C', text: 'Option c' },
+  { key: 'D', text: 'Option d' },
+  { key: 'E', text: 'Option e' },
+  { key: 'F', text: 'Option f' },
+  { key: 'G', text: 'Option g' }
+  ]
 class App extends Component {
   render() {
     return (
-      <main>
-        <Tab />
-      </main>
+      <div className="App">
+        <Dropdown data={data}/>
+      </div>
     );
   }
 }
